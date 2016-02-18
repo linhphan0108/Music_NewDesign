a1e7238f20537e015e41e4d316811a316e94365d
linh phan <linhphan0108@gmail.com>
2016-02-18 20:39:19 +0700

    center the content of the small notification

--- app/src/main/java/com/linhphan/music/ui/fragment/BaseMusicFragment.java
+++ app/src/main/java/com/linhphan/music/ui/fragment/BaseMusicFragment.java
@@ -1,10 +1,15 @@
 package com.linhphan.music.ui.fragment;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.os.Handler;
 
 import com.linhphan.androidboilerplate.ui.fragment.BaseFragment;
 import com.linhphan.music.ui.activity.BaseMusicActivity;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * Created by linhphan on 11/18/15.
  */
@@ -15,5 +20,23 @@ public abstract class BaseMusicFragment extends BaseFragment {
         super.onAttach(context);
         BaseMusicActivity baseActivity = (BaseMusicActivity) context;
         mBaseHandler = baseActivity.getBaseHandler();
+    }
+
+    public static BaseFragment newInstance(Class<?> c, Bundle bundle){
+        BaseFragment baseFragment = null;
+        try {
+            Constructor<?> constructor = c.getConstructors()[0];
+            baseFragment = (BaseFragment) constructor.newInstance();
+            if (bundle != null){
+                baseFragment.setArguments(bundle);
+            }
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (java.lang.InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return baseFragment;
     }
 }
